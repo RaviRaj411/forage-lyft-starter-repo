373e302180102ffe81b42f660095fe3874f43bab
Ravi Raj Suman <124388623+RaviRaj411@users.noreply.github.com>
2023-08-13 00:35:11 +0530

    initial commit

---
+++ engine/willoughby_engine.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+from car import Car
+
+
+class WilloughbyEngine():
+    def __init__(self, current_mileage, last_service_mileage):
+        
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def engine_should_be_serviced(self):
+        return self.current_mileage - self.last_service_mileage > 60000
